3e1ddd43cd2e5c4b337b232006b3389742d49629
Nikola Gospodinov <nikola96gospodinov@gmail.com>
2019-05-16 12:02:11 +0100

    Updates

---
+++ precache-manifest.a0afadc3b2f1334ec11f5cce5590fc16.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ffa517e6be9a245677f",
+    "url": "/portfolioUpdated/static/css/main.5e219f9d.chunk.css"
+  },
+  {
+    "revision": "7ffa517e6be9a245677f",
+    "url": "/portfolioUpdated/static/js/main.fe8ddf2a.chunk.js"
+  },
+  {
+    "revision": "848acdf875bb7b350cbd",
+    "url": "/portfolioUpdated/static/js/runtime~main.ac2905d6.js"
+  },
+  {
+    "revision": "fb94fe1c4f9f4b864752",
+    "url": "/portfolioUpdated/static/js/2.08a1d382.chunk.js"
+  },
+  {
+    "revision": "39758cfcb42ab4a93f659dd632613d7b",
+    "url": "/portfolioUpdated/static/media/logo_transparent.39758cfc.png"
+  },
+  {
+    "revision": "a13d3e4ea75af2c18c74a1f44873db3b",
+    "url": "/portfolioUpdated/static/media/hero.a13d3e4e.jpg"
+  },
+  {
+    "revision": "797f876b170393987f8e09839a40176d",
+    "url": "/portfolioUpdated/static/media/heroAbout.797f876b.jpg"
+  },
+  {
+    "revision": "d6e246960d900b51064427fd7db85f4b",
+    "url": "/portfolioUpdated/static/media/SkillsHero.d6e24696.jpg"
+  },
+  {
+    "revision": "5d09d1188e79207d39004a26f7a9b794",
+    "url": "/portfolioUpdated/static/media/Story.5d09d118.jpg"
+  },
+  {
+    "revision": "03272cbb98ac1e3a4d4ba14287ac4637",
+    "url": "/portfolioUpdated/static/media/projectHero.03272cbb.jpg"
+  },
+  {
+    "revision": "60d5935b0e40f110229b3ba7712ebbd2",
+    "url": "/portfolioUpdated/static/media/nutri.60d5935b.jpg"
+  },
+  {
+    "revision": "f30a28da49889a97a96d4ef60c7bdaef",
+    "url": "/portfolioUpdated/static/media/THC.f30a28da.jpg"
+  },
+  {
+    "revision": "638b601c6569d918437872e37491ef53",
+    "url": "/portfolioUpdated/static/media/Portfolio.638b601c.jpg"
+  },
+  {
+    "revision": "d8ccde348f755e5dcfc0b752c815ae27",
+    "url": "/portfolioUpdated/static/media/aboutMe.d8ccde34.jpg"
+  },
+  {
+    "revision": "797b7cb979dcf18c315486c8854e2fe7",
+    "url": "/portfolioUpdated/index.html"
+  }
+];
